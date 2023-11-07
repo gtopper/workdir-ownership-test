44d5c991c74980012766519a363d8ac340b317ed
Gal Topper <gal.topper@gmail.com>
2023-11-07 21:26:07 +0800

    Comment out project

--- spark_job.py
+++ spark_job.py
@@ -1,7 +1,7 @@
 # not really using the project, but still checking if it can be loaded
-project: MlrunProject = mlrun.get_or_create_project(
-        name="pyspark-iris", context="./", user_project=True
-    )
+#project: MlrunProject = mlrun.get_or_create_project(
+#        name="pyspark-iris", context="./", user_project=True
+#    )
 read_obj = SparkSession.builder.getOrCreate().read
 # weirdly, v3io:///projects/ is automatically added to the head of the url
 url = "pyspark-iris-alp-aribal/artifacts/example_iris_data.csv"
