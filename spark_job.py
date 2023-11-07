4b685ed50f4a6b901a25966ac54c0353e1f4918d
Gal Topper <gal.topper@gmail.com>
2023-11-07 21:51:44 +0800

    Uncomment, add missing imports

--- spark_job.py
+++ spark_job.py
@@ -1,7 +1,11 @@
+import mlrun
+from mlrun.projects import MlrunProject
+from pyspark.sql import SparkSession
+
 # not really using the project, but still checking if it can be loaded
-#project: MlrunProject = mlrun.get_or_create_project(
-#        name="pyspark-iris", context="./", user_project=True
-#    )
+project: MlrunProject = mlrun.get_or_create_project(
+    name="pyspark-iris", context="./", user_project=True
+)
 read_obj = SparkSession.builder.getOrCreate().read
 # weirdly, v3io:///projects/ is automatically added to the head of the url
 url = "pyspark-iris-alp-aribal/artifacts/example_iris_data.csv"
