52a230e0d6fb8316901bbeaad4f9b164263914ca
Gal Topper <gal.topper@gmail.com>
2023-11-09 11:55:55 +0800

    Update spark_job.py

--- spark_job.py
+++ spark_job.py
@@ -7,8 +7,7 @@ project: MlrunProject = mlrun.get_or_create_project(
     name="pyspark-iris", context="./", user_project=True
 )
 read_obj = SparkSession.builder.getOrCreate().read
-# weirdly, v3io:///projects/ is automatically added to the head of the url
-url = "pyspark-iris-alp-aribal/artifacts/example_iris_data.csv"
+url = "v3io://bigdata/example_iris_data.csv"
 print(f"{url=}")
 df = read_obj.load(url, format="csv")
 print(f"{df=}")
